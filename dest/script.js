ca173d4f867ecf8f6d20b7550f035d98b5bf317a
bishwasjungbist <bishwasjungbist@gmail.com>
2026-01-20 22:46:43 +0000

    learned basic of object properties

--- dest/script.js
+++ dest/script.js
@@ -1,38 +1,9 @@
-console.log('hel');
-const list = document.querySelector('#list');
-const form = document.getElementById('new-task-form');
-const input = document.querySelector('#new-task-title');
-console.log("form:", form);
-console.log("input:", input);
-console.log("list:", list);
-form?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log('no refresh');
-    if (form?.value == "" || input?.value == null)
-        return;
-    /*FOR TODO */
-    const newTask = {
-        /*means the variable is a task obkect*/
-        id: crypto.randomUUID(),
-        title: input.value,
-        completed: false,
-        createdAt: new Date()
-    };
-    addListItem(newTask);
-});
-function addListItem(task) {
-    /*this mean it will receve data in taskData object
-    format from newTask which auto gen the data
-    and will get saved into task*/
-    const item = document.createElement('li');
-    const label = document.createElement('label');
-    const checkbox = document.createElement('input');
-    checkbox.type = "checkbox";
-    label.append(checkbox, task.title);
-    /*porbaly creates box infront of the task(title)*/
-    item.append(label);
-    list?.append(item);
-    /*what? isnt item already a list, and list can be null*/
-}
+var numbers = 12;
+console.log("hello world");
+const obj = { width: 10, height: 15, yolo: 12 };
+const area = obj.width * obj.yolo * numbers; //so I dont have to keep defining huh
+console.log(area);
+//yolo = 23 at cost of not being able to use it outside
+console.log(numbers);
 export {};
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,8 @@
-console.log("helloword")
+ /*this was me learning the properities of objects */
+// var numbers = 12;
+// console.log("hello world")
+// const obj = {width : 10, height: 15 , yolo: 12};
+// const area = obj.width*obj.yolo*numbers; //so I dont have to keep defining huh
+// console.log(area);
+// //yolo = 23 at cost of not being able to use it outside
+// console.log(numbers);
